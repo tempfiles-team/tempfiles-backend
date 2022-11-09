982863ef8f20b130fd0543f11852ebf21d6751fe
minpeter <kali2005611@gmail.com>
2022-11-09 11:38:45 +0900

    fix bug - delete filename if 한국어 & 공백

--- file/delete.go
+++ file/delete.go
@@ -1,32 +1,35 @@
 package file
 
 import (
+	"log"
+	"net/url"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/minio/minio-go/v7"
 	"golang.org/x/net/context"
 )
 
-func delete(objectName string) (fiber.Map, error) {
-
-	err := MinioClient.RemoveObject(context.Background(), BucketName, objectName, minio.RemoveObjectOptions{})
-
-	if err != nil {
-		return nil, err
-	}
-	return fiber.Map{
-		"message": "delete success",
-		"success": true,
-	}, nil
-}
-
 func DeleteHandler(c *fiber.Ctx) error {
-	fileName := c.Params("filename")
-	result, err := delete(fileName)
+
+	fileName, err := url.QueryUnescape(c.Params("filename"))
+
 	if err != nil {
 		return c.Status(500).JSON(fiber.Map{
 			"message": "minio delete error",
 			"error":   err.Error(),
 		})
 	}
-	return c.JSON(result)
+
+	if err := MinioClient.RemoveObject(context.Background(), BucketName, fileName, minio.RemoveObjectOptions{}); err != nil {
+		return c.Status(500).JSON(fiber.Map{
+			"message": "minio delete error",
+			"error":   err.Error(),
+		})
+	}
+	log.Printf("Successfully deleted %s\n", fileName)
+
+	return c.JSON(fiber.Map{
+		"message": "delete success",
+		"success": true,
+	})
 }
